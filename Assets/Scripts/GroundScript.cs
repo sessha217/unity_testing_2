cfc25825b935d6cbe86f68159dd4a09cd2b31811
sessha217 <sessha217@gmail.com>
2014-07-28 10:42:26 -0400

    nice

---
+++ Assets/Scripts/GroundScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundScript : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTouchDown( Vector3 hit ) {
+		Vector3 towardsPlayer = hit - player.transform.localScale;
+		player.TouchController.MoveTowards(towardsPlayer);
+	}
+}
